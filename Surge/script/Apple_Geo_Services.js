4b03104933b5b8e0d5bb560a18cc1208e738c186
wang95 <wang95@vip.qq.com>
2021-11-10 10:07:55 +0800

    add Apple Services

---
+++ Surge/script/Apple_Geo_Services.js
@@ -0,0 +1,44 @@
+// 判断是否是重写
+const isRequest = typeof $request != "undefined";
+// 判断是否是Surge
+const isSurge = typeof $httpClient != "undefined";
+// 判断是否是QuanX
+const isQuanX = typeof $task != "undefined";
+// 判断是否是Loon
+const isLoon = typeof $loon != "undefined";
+
+/*
+README:https://github.com/VirgilClyne/iRingo
+*/
+
+let GeoCountryCode = "US";
+
+if (typeof $argument != "undefined") {
+    let arg = Object.fromEntries($argument.split("&").map((item) => item.split("=")));
+    console.log(JSON.stringify(arg));
+    GeoCountryCode = arg.GeoCountryCode;
+};
+
+const url = $request.url;
+
+const path1 = "/pep/gcc";
+
+if (url.indexOf(path1) != -1) {
+    var today = new Date();
+    var UTCstring = today.toUTCString();
+    var response = {};
+    if (isQuanX) response.status = 'HTTP/1.1 200 OK';
+    if (isSurge || isLoon) response.status = 200;
+    response.headers = {
+        'Content-Type': 'text/html',
+        'Date': UTCstring,
+        'Connection': 'keep-alive',
+        'Content-Encoding': 'identity'
+    };
+    response.body = GeoCountryCode;
+    if (isQuanX) $done(response);
+    if (isSurge || isLoon) $done({response});
+}
+else {
+    done({})
+}
