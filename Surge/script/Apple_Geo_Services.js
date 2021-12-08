bbef04669c2d55aad6fb10c126a023dc239d5852
wang95 <wang95@vip.qq.com>
2021-12-08 10:10:46 +0800

    update Apple Geo Services

--- Surge/script/Apple_Geo_Services.js
+++ Surge/script/Apple_Geo_Services.js
@@ -1,18 +1,26 @@
 // 判断是否是重写
 const isRequest = typeof $request != "undefined";
+const isResponse = typeof $response != "undefined";
 // 判断是否是Surge
 const isSurge = typeof $httpClient != "undefined";
 // 判断是否是QuanX
 const isQuanX = typeof $task != "undefined";
 // 判断是否是Loon
 const isLoon = typeof $loon != "undefined";
+// 关闭请求
+const done = (value = {}) => {
+    if (isQuanX) return $done(value);
+    if (isSurge) isRequest ? $done(value) : $done();
+};
 
 /*
 README:https://github.com/VirgilClyne/iRingo
 */
 
+// Default GeoCountryCode: US
 let GeoCountryCode = "US";
 
+// Argument Function Supported
 if (typeof $argument != "undefined") {
     let arg = Object.fromEntries($argument.split("&").map((item) => item.split("=")));
     console.log(JSON.stringify(arg));
@@ -24,11 +32,10 @@ const url = $request.url;
 const path1 = "/pep/gcc";
 
 if (url.indexOf(path1) != -1) {
+    console.log(path1);
     var today = new Date();
     var UTCstring = today.toUTCString();
     var response = {};
-    if (isQuanX) response.status = 'HTTP/1.1 200 OK';
-    if (isSurge || isLoon) response.status = 200;
     response.headers = {
         'Content-Type': 'text/html',
         'Date': UTCstring,
@@ -36,9 +43,12 @@ if (url.indexOf(path1) != -1) {
         'Content-Encoding': 'identity'
     };
     response.body = GeoCountryCode;
-    if (isQuanX) $done(response);
-    if (isSurge || isLoon) $done({response});
-}
-else {
-    done({})
-}
+    if (isQuanX) {
+        response.status = "HTTP/1.1 200 OK";
+        done(response);
+    }
+    if (isSurge || isLoon) {
+        response.status = 200;
+        done({ response });
+    }
+} else done();
